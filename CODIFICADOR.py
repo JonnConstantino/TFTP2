8047ff918694ebbe4e95d83a99c5e2532a7daa37
Jhonatan Constantino <jhonatan.c16@aluno.ifsc.edu.br>
2022-07-23 09:05:06 -0300

    implementado codificador e decodificador para os novos tipos de mensagens

--- CODIFICADOR.py
+++ CODIFICADOR.py
@@ -1,5 +1,5 @@
 import flatbuffers
-from exemplo import Ack, Data, Request
+from exemplo import Ack, Data, Request, Error, List, Mkdir, Move, Elementos, Arquivo, Pasta
 
 def codifica_ack():
     # cria um construtor do flatbuffers
@@ -74,6 +74,153 @@ def codifica_data():
     # carrega as informações do construtor em um buffer
     return builder.Output()
 
+def codifica_error():
+    # cria um construtor do flatbuffers
+    builder = flatbuffers.Builder(1024)
+
+    # cria uma string no contrutor
+    errormsg = builder.CreateString('ERROR message')
+
+    # inicia objeto Ativo criado anteriormente com arquivo .fbs
+    Error.ErrorStart(builder)
+
+    # adiciona os atributos nome, id, valor e timestamp
+    Error.ErrorAddOpcode(builder, 1)
+    Error.ErrorAddErrorcode(builder, 2)
+    Error.ErrorAddErrmsg(builder, errormsg)
+
+    # finaliza o objeto Ativo
+    msg = Error.ErrorEnd(builder)
+
+    # finaliza o construtor com a mensagem
+    builder.Finish(msg)
+
+    # carrega as informações do construtor em um buffer
+    return builder.Output()
+
+def codifica_list():
+    # cria um construtor do flatbuffers
+    builder = flatbuffers.Builder(1024)
+
+    # cria uma string no contrutor
+    caminho = builder.CreateString('/home/')
+    nome_arquivo = builder.CreateString('arquivo.txt')
+    nome_pasta = builder.CreateString('pasta')
+    errormsg = builder.CreateString('ERROR msg')
+
+    # inicia os objetos criados anteriormente com arquivo .fbs
+    Arquivo.ArquivoStart(builder)
+
+    # adiciona os atributos
+    Arquivo.ArquivoAddNome(builder, nome_arquivo)
+    Arquivo.ArquivoAddTamanho(builder, 10)
+
+    arquivo = Arquivo.ArquivoEnd(builder)
+
+    Pasta.PastaStart(builder)
+
+    Pasta.PastaAddNome(builder, nome_pasta)
+
+    pasta = Pasta.PastaEnd(builder)
+
+    Elementos.ElementosStart(builder)
+
+    Elementos.ElementosAddArquivo(builder, arquivo)
+    Elementos.ElementosAddPasta(builder, pasta)
+
+    elementos = Elementos.ElementosEnd(builder)
+
+    Error.ErrorStart(builder)
+
+    Error.ErrorAddOpcode(builder, 1)
+    Error.ErrorAddErrorcode(builder, 2)
+    Error.ErrorAddErrmsg(builder, errormsg)
+
+    error = Error.ErrorEnd(builder)
+
+    List.ListStart(builder)
+
+    List.ListAddOpcode(builder, 1)
+    List.ListAddCaminho(builder, caminho)
+    List.ListAddElementos(builder, elementos)
+    List.ListAddError(builder, error)
+
+    # finaliza o objeto da mensagem
+    msg = List.ListEnd(builder)
+
+    # finaliza o construtor com a mensagem
+    builder.Finish(msg)
+
+    # carrega as informações do construtor em um buffer
+    return builder.Output()
+
+def codifica_mkdir():
+    # cria um construtor do flatbuffers
+    builder = flatbuffers.Builder(1024)
+
+    # cria uma string no contrutor
+    caminho = builder.CreateString('/home/')
+    errormsg = builder.CreateString('ERROR msg')
+
+    # inicia os objetos criados anteriormente com arquivo .fbs
+    
+    Error.ErrorStart(builder)
+
+    Error.ErrorAddOpcode(builder, 1)
+    Error.ErrorAddErrorcode(builder, 2)
+    Error.ErrorAddErrmsg(builder, errormsg)
+
+    error = Error.ErrorEnd(builder)
+
+    Mkdir.MkdirStart(builder)
+
+    Mkdir.MkdirAddOpcode(builder, 1)
+    Mkdir.MkdirAddCaminho(builder, caminho)
+    Mkdir.MkdirAddError(builder, error)
+
+    # finaliza o objeto da mensagem
+    msg = Mkdir.MkdirEnd(builder)
+
+    # finaliza o construtor com a mensagem
+    builder.Finish(msg)
+
+    # carrega as informações do construtor em um buffer
+    return builder.Output()
+
+def codifica_move():
+    # cria um construtor do flatbuffers
+    builder = flatbuffers.Builder(1024)
+
+    # cria uma string no contrutor
+    nome_original = builder.CreateString('nome_original.txt')
+    novo_nome = builder.CreateString('novo_nome.txt')
+    errormsg = builder.CreateString('ERROR msg')
+
+    # inicia os objetos criados anteriormente com arquivo .fbs
+    Error.ErrorStart(builder)
+
+    Error.ErrorAddOpcode(builder, 1)
+    Error.ErrorAddErrorcode(builder, 2)
+    Error.ErrorAddErrmsg(builder, errormsg)
+
+    error = Error.ErrorEnd(builder)
+
+    Move.MoveStart(builder)
+
+    Move.MoveAddOpcode(builder, 1)
+    Move.MoveAddNomeOriginal(builder, nome_original)
+    Move.MoveAddNovoNome(builder, novo_nome)
+    Move.MoveAddError(builder, error)
+
+    # finaliza o objeto da mensagem
+    msg = Move.MoveEnd(builder)
+
+    # finaliza o construtor com a mensagem
+    builder.Finish(msg)
+
+    # carrega as informações do construtor em um buffer
+    return builder.Output()
+
 def escrever_arquivo(nomearquivo, buffer):
     # escreve o buffer em um arquivo
     try:
@@ -86,3 +233,7 @@ def escrever_arquivo(nomearquivo, buffer):
 escrever_arquivo('ack', codifica_ack())
 escrever_arquivo('req', codifica_request())
 escrever_arquivo('data', codifica_data())
+escrever_arquivo('error', codifica_error())
+escrever_arquivo('list', codifica_list())
+escrever_arquivo('mkdir', codifica_mkdir())
+escrever_arquivo('move', codifica_move())
--- DECODIFICADOR.py
+++ DECODIFICADOR.py
@@ -1,15 +1,20 @@
+import argparse
 import sys
-from exemplo import Ack, Data, Request
+from exemplo import Ack, Data, Request, Error, List, Mkdir, Move, Elementos, Arquivo, Pasta
 
-def decode_ack(nomearquivo):
-    # lê o arquivo e salva o conteúdo em um buffer
+def bufferiza(nomearquivo):
     try:
         file = open("./"+nomearquivo, "rb")
         buf = file.read()
         file.close()
+        return buf
     except:
         print('Erro ao ler o arquivo')
         sys.exit()
+
+def decode_ack(nomearquivo):
+    # lê o arquivo e salva o conteúdo em um buffer
+    buf = bufferiza(nomearquivo)
 
     msg = Ack.Ack.GetRootAsAck(buf, 0)
 
@@ -24,13 +29,7 @@ def decode_ack(nomearquivo):
 
 def decode_req(nomearquivo):
     # lê o arquivo e salva o conteúdo em um buffer
-    try:
-        file = open("./"+nomearquivo, "rb")
-        buf = file.read()
-        file.close()
-    except:
-        print('Erro ao ler o arquivo')
-        sys.exit()
+    buf = bufferiza(nomearquivo)
 
     msg = Request.Request.GetRootAsRequest(buf, 0)
 
@@ -47,13 +46,7 @@ def decode_req(nomearquivo):
 
 def decode_data(nomearquivo):
     # lê o arquivo e salva o conteúdo em um buffer
-    try:
-        file = open("./"+nomearquivo, "rb")
-        buf = file.read()
-        file.close()
-    except:
-        print('Erro ao ler o arquivo')
-        sys.exit()
+    buf = bufferiza(nomearquivo)
 
     msg = Data.Data.GetRootAsData(buf, 0)
 
@@ -68,19 +61,111 @@ def decode_data(nomearquivo):
     print('block =', block)
     print('data =', data)
 
-# lê o nome do arquivo como argumento da linha de comando
-try:
-    nomearq = sys.argv[1]
-    tipomsg = sys.argv[2]
-except:
-    print('Erro: python3 %s nome_do_arquivo tipo_de_mensagem' % sys.argv[0])
-    sys.exit()
+def decode_error(nomearquivo):
+    # lê o arquivo e salva o conteúdo em um buffer
+    buf = bufferiza(nomearquivo)
 
-if tipomsg == 'ack':
-    decode_ack(nomearq)
-elif tipomsg == 'req':
-    decode_req(nomearq)
-elif tipomsg == 'data':
-    decode_data(nomearq)
+    msg = Error.Error.GetRootAsError(buf, 0)
+
+    # salva as informações em variáveis
+    opcode = msg.Opcode()
+    errorcode = msg.Errorcode()
+    errmsg = msg.Errmsg()
+
+    # mostra na tela
+    print('Mensagem decodificada:')
+    print('opcode =', opcode)
+    print('errorcode =', errorcode)
+    print('errmsg =', errmsg)
+
+def decode_list(nomearquivo):
+    # lê o arquivo e salva o conteúdo em um buffer
+    buf = bufferiza(nomearquivo)
+
+    msg = List.List.GetRootAsList(buf, 0)
+
+    # salva as informações em variáveis
+    opcode = msg.Opcode()
+    caminho = msg.Caminho()
+    elementos = msg.Elementos()
+    error = msg.Error()
+
+    # mostra na tela
+    print('Mensagem decodificada:')
+    print('opcode =', opcode)
+    print('caminho =', caminho)
+    print('elemento: nome do arquivo =', elementos.Arquivo().Nome())
+    print('elemento: tamanho do arquivo =', elementos.Arquivo().Tamanho())
+    print('elemento: nome da pasta =', elementos.Pasta().Nome())
+    print('error: opcode = ', error.Opcode())
+    print('error: errorcode = ', error.Errorcode())
+    print('error: errormsg = ', error.Errmsg())
+
+def decode_mkdir(nomearquivo):
+    # lê o arquivo e salva o conteúdo em um buffer
+    buf = bufferiza(nomearquivo)
+
+    msg = Mkdir.Mkdir.GetRootAsMkdir(buf, 0)
+
+    # salva as informações em variáveis
+    opcode = msg.Opcode()
+    caminho = msg.Caminho()
+    error = msg.Error()
+
+    # mostra na tela
+    print('Mensagem decodificada:')
+    print('opcode =', opcode)
+    print('caminho =', caminho)
+    print('error: opcode = ', error.Opcode())
+    print('error: errorcode = ', error.Errorcode())
+    print('error: errormsg = ', error.Errmsg())
+
+def decode_move(nomearquivo):
+    # lê o arquivo e salva o conteúdo em um buffer
+    buf = bufferiza(nomearquivo)
+
+    msg = Move.Move.GetRootAsMove(buf, 0)
+
+    # salva as informações em variáveis
+    opcode = msg.Opcode()
+    nome_original = msg.NomeOriginal()
+    novo_nome = msg.NovoNome()
+    error = msg.Error()
+
+    # mostra na tela
+    print('Mensagem decodificada:')
+    print('opcode =', opcode)
+    print('nome original =', nome_original)
+    print('novo nome =', novo_nome)
+    print('error: opcode = ', error.Opcode())
+    print('error: errorcode = ', error.Errorcode())
+    print('error: errormsg = ', error.Errmsg())
+
+# lê o nome do arquivo como argumento da linha de comando
+parser = argparse.ArgumentParser()
+parser.add_argument('nomearquivo', help='define o nome do arquivo a ser decodificado')
+parser.add_argument('--ack', help='mensagem do tipo ack', action='store_true')
+parser.add_argument('--req', help='mensagem do tipo req', action='store_true')
+parser.add_argument('--data', help='mensagem do tipo data', action='store_true')
+parser.add_argument('--error', help='mensagem do tipo error', action='store_true')
+parser.add_argument('--list', help='mensagem do tipo list', action='store_true')
+parser.add_argument('--mkdir', help='mensagem do tipo mkdir', action='store_true')
+parser.add_argument('--move', help='mensagem do tipo move', action='store_true')
+args = parser.parse_args()
+
+if args.ack:
+    decode_ack(args.nomearquivo)
+elif args.req:
+    decode_req(args.nomearquivo)
+elif args.data:
+    decode_data(args.nomearquivo)
+elif args.error:
+    decode_error(args.nomearquivo)
+elif args.list:
+    decode_list(args.nomearquivo)
+elif args.mkdir:
+    decode_mkdir(args.nomearquivo)
+elif args.move:
+    decode_move(args.nomearquivo)
 else:
-    print('Tipo de mensagem não reconhecido')
+    print('Defina o tipo de mensagem como argumento')
